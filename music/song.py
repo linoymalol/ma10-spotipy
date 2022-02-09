08f378affcefd9990e8e00f5cab9ea6058770b0d
Linoy Malol <linoymalol1@gmail.com>
2022-02-09 12:47:09 +0200

    changed class

--- music/song.py
+++ music/song.py
@@ -1,4 +1,8 @@
 class Song:
-    def __init__(self, id, name):
+    def __init__(self, id, name, popularity):
         self.id = id
         self.name = name
+        self.popularity = popularity
+
+    def __repr__(self):
+        return f'song id: {self.id} name: {self.name} popularity: {self.popularity}'
